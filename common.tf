78361d45a32c9be4f4ce53e0c57f0ebf31c1cb71
Nemo <me@captnemo.in>
2019-02-10 21:51:27 +0530

    Bump the docker-api ignition module

--- common.tf
+++ common.tf
@@ -1,15 +1,14 @@
-module "docker-api" {
+# This file is shared between all provisioners (digital ocean/aws/...)
+module "ignition" {
   source  = "captn3m0/docker-api/ignition"
-  version = "1.0.0"
-
-  cert_path = "${var.cert_path}"
-
-  client_subject = {
-    common_name = "client.docker.${var.domain}"
-  }
+  version = "1.0.1"
 
   ca_subject = {
     common_name = "ca.docker.${var.domain}"
+  }
+
+  client_subject = {
+    common_name = "client.docker.${var.domain}"
   }
 
   server_subject = {
